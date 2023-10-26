d2db5c8e37918725cfde0e35a2a5095bd2aeeab5
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-10-26 19:08:06 +0400

    feat(1): added user router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const api = require('./routes/api');
+
 const app = express();
+
+app.use('/api', api);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from KidoVerse powerful server.</h1>');
