daadb92f63114c320a5b0127379d00ea3c2cd786
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-10-26 19:01:47 +0400

    feat(1): created User model

---
+++ src/models/users/users.mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isEmailVerified: {
+    type: Boolean,
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: String,
+  },
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+});
+
+module.exports = mongoose.model('User', usersSchema);
