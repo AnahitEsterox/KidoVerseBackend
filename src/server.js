1b92ee233a902d87495269ea77dc1c0e61bc2bc1
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-10-26 18:53:09 +0400

    feat(1): connected mongodb

--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ const http = require('http');
 require('dotenv').config();
 
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+async function startServer() {
+  await mongoConnect();
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+  });
+}
+
+startServer();
